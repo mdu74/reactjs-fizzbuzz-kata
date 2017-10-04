3a9ccc39a87b744564de88ba84220f74d492dd59
mdu74 <mdu74.C@gmail.com>
2017-10-04 15:06:49 +0200

    adding user interaction to my fizzbuzz

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< HEAD
 class App extends Component {
   isfizzbuzz(input){
     if(input % 3 === 0)
@@ -20,9 +21,61 @@ class App extends Component {
         <p className="App-intro">
           Is this fizz or buzz or even both?
         </p>
+=======
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fizzOrBuzz: ''
+    };
+  }
+
+  _setState = (state) => {
+    this.setState(state, () => {});
+  }
+
+  _divisibleBy = (input, divisableBy) => {
+    return input % divisableBy === 0;
+  }
+
+  isfizzbuzz = (event) => {
+    const input = event.target.value;
+
+    if(this._divisibleBy(input, 3)){
+      this._setState({ fizzOrBuzz: 'Fizz' });
+      return ;
+    }
+
+    if(this._divisibleBy(input, 5)){
+      this._setState({ fizzOrBuzz: 'Buzz' });
+      return;
+    }
+
+    this._setState({ fizzOrBuzz: input });
+}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Fizzbuzz</h1>
+        </header>
+        <p className="App-intro">
+          Is this <b>Fizz</b> or <b>Buzz</b> or even <b>FizzBuzz</b>?
+        </p>
+
+        <input type="text" id="name" onChange={this.isfizzbuzz} />
+
+         <h3 className="fizz-or-buzz">{ this.state.fizzOrBuzz }</h3>
+
+>>>>>>> adding user interaction to my fizzbuzz
       </div>
     );
   }
 }
+<<<<<<< HEAD
 
 export default App;
+=======
+>>>>>>> adding user interaction to my fizzbuzz
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 import App from './App';
 
 it('renders without crashing', () => {
@@ -55,3 +56,70 @@ it('Given 5 should return Buzz', () => {
 function CreateApp(){
   return new App();
 }
+=======
+import ReactTestUtils from 'react-dom/test-utils';
+import App from './App';
+
+describe('App', ()=>{
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App />, div);
+  });
+
+  const notDivisibleBy3and5 = [1,2,4,7,8];
+  const divisibleBy3 = [3,6,9,12];
+  const divisibleBy5 = [5,10,20];
+
+  fit('Given numbers not divisible by both 3 and 5 should return number', () => {
+    //---------------Set up test pack-------------------
+    var event = {
+      target: 0
+    };
+    //var input = event.target;
+
+    var fizzBuzzApp = CreateApp();
+    var expected = 0;
+    var fizzBuzzHeader = ReactTestUtils.renderIntoDocument(<App />);
+    //notDivisibleBy3and5.map((fizzBuzzNumber) => {
+    //  event.target = fizzBuzzNumber;
+    //  expected = fizzBuzzNumber;
+    //});
+    //---------------Execute Test ----------------------
+    fizzBuzzApp.isfizzbuzz(event);
+    var fizzOrBuzzClass = ReactTestUtils.findRenderedDOMComponentWithClass(fizzBuzzHeader, 'fizz-or-buzz');
+    //---------------Test Result -----------------------
+    expect("Through my fizzBuzzApp: ", fizzOrBuzzClass.textContent).toBe(expected);
+  });
+
+  it('Given numbers divisible by 3 should return Fizz', () => {
+    //---------------Set up test pack-------------------
+    var input = 0;
+    var fizzBuzz = CreateApp();
+    divisibleBy3.map((fizzBuzzNumber) => {
+      input = fizzBuzzNumber;
+    });
+    //---------------Execute Test ----------------------
+    var result = fizzBuzz.isfizzbuzz(input);
+    //---------------Test Result -----------------------
+    expect(result).toBe('Fizz');
+  });
+
+  it('Given numbers divisible by 5 should return Buzz', () => {
+    //---------------Set up test pack-------------------
+    var input = 0;
+    var fizzBuzz = CreateApp();
+    divisibleBy5.map((fizzBuzzNumber) => {
+      input = fizzBuzzNumber;
+    });
+    //---------------Execute Test ----------------------
+    var result = fizzBuzz.isfizzbuzz(input);
+    //---------------Test Result -----------------------
+    expect(result).toBe('Buzz');
+  });
+
+  function CreateApp(){
+    return new App();
+  }
+});
+>>>>>>> adding user interaction to my fizzbuzz
